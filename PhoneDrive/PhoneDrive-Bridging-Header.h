01d0bfa3f9a12ee04a002ad79fe841d700291130
Jan Seredynski <janseredynski@gmail.com>
2021-12-01 18:50:32 +0100

    First commit

---
+++ PhoneDrive/PhoneDrive-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+
+#include "TcpClient.h"
+#include "TlsClient.h"
+#include "FileManager.h"
