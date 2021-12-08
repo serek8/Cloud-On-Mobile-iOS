ff356cab9b6561a7051f1deb2d5d0f08da808665
Karol Piątek <karol.piatek@netguru.com>
2021-12-08 20:27:00 +0100

    Run SwiftFormat

--- PhoneDrive/ViewController.swift
+++ PhoneDrive/ViewController.swift
@@ -8,78 +8,69 @@
 import UIKit
 
 class ViewController: UIViewController, CommandManagerDelegate {
-  func serverOnReconnecting() {
-    print("reconnecting")
-  }
-  
-  
-  @IBOutlet weak var switchServerTarget: UISwitch!
-  func serverOnClose() {
-    DispatchQueue.main.async {
-        self.labelCodeValue.text = "serverOnClose"
+    func serverOnReconnecting() {
+        print("reconnecting")
     }
-  }
-  
-  func getServerUrl() -> String {
-    if self.switchServerTarget.isOn{
-      return "seredynski.com"
+
+    @IBOutlet var switchServerTarget: UISwitch!
+    func serverOnClose() {
+        DispatchQueue.main.async {
+            self.labelCodeValue.text = "serverOnClose"
+        }
     }
-    else{
-      return "192.168.50.10"
+
+    func getServerUrl() -> String {
+        if switchServerTarget.isOn {
+            return "seredynski.com"
+        } else {
+            return "192.168.50.10"
+        }
     }
-  }
-  
-//  func getServerUrl() -> String {
+
+    //  func getServerUrl() -> String {
 //    if self.switchServerTarget.isEnabled{
 //      return "seredynski.com"
 //    }
 //    else{
 //      return "192.168.50.10"
 //    }
-//  }
-  
-  func serverOnConnected(passocde: Int) {
-    DispatchQueue.main.async {
-        self.labelCodeValue.text = "\(passocde)"
-        UIApplication.shared.isIdleTimerDisabled = true
+    //  }
+
+    func serverOnConnected(passocde: Int) {
+        DispatchQueue.main.async {
+            self.labelCodeValue.text = "\(passocde)"
+            UIApplication.shared.isIdleTimerDisabled = true
+        }
     }
-  }
-  
-  func serverOnReconnected() {
-    DispatchQueue.main.async {
-        self.labelCodeValue.text = "serverOnReconnected"
+
+    func serverOnReconnected() {
+        DispatchQueue.main.async {
+            self.labelCodeValue.text = "serverOnReconnected"
+        }
     }
-  }
-  
-    
+
     func serverOnNewMessage(data: Data) {
         DispatchQueue.main.async {
             self.labelCodeValue.text = String(data: data, encoding: .ascii)
         }
     }
-    
-    func serverOnClose(status: Int) {
-        
-        
-    }
-    
-    @IBOutlet weak var labelCodeValue: UILabel!
-    
+
+    func serverOnClose(status: Int) {}
+
+    @IBOutlet var labelCodeValue: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 //      tlsHanshake()
-      CommandManager.shared.delegate = self
+        CommandManager.shared.delegate = self
         // Do any additional setup after loading the view.
-      
-      CommandManager.shared.listFiles()
-      
-    }
 
+        CommandManager.shared.listFiles()
+    }
 
     @IBAction func buttonConnectClicked(_ sender: UIButton) {
-      CommandManager.shared.ip = getServerUrl()
-      self.switchServerTarget.isEnabled = false
-      CommandManager.shared.connect()
+        CommandManager.shared.ip = getServerUrl()
+        switchServerTarget.isEnabled = false
+        CommandManager.shared.connect()
     }
 }
-
