eae3c621b7197d75938c1617d1f4af65264038da
Jan Seredynski <janseredynski@gmail.com>
2021-12-15 09:47:27 +0100

    Refactor and swiftformat

--- CloudOnMobile/CoreWrapper/CoreWrapperSwift.swift
+++ CloudOnMobile/CoreWrapper/CoreWrapperSwift.swift
@@ -6,14 +6,11 @@
 
 import Foundation
 
-
-@_cdecl("coreDidDownlodFile")
-func coreDidDownlodFile(filepath: UnsafePointer<UInt8>) {
-  let filepathStr = String.init(cString: filepath)
-  CommandManager.shared.delegate?.serverOnFileDownlaoded(filepath: filepathStr)
+@_cdecl("coreDidDownlodFile") func coreDidDownlodFile(filepath: UnsafePointer<UInt8>) {
+    let filepathStr = String(cString: filepath)
+    CommandManager.shared.delegate?.serverOnFileDownlaoded(filepath: filepathStr)
 }
 
-
-func initSharedCore(){
-  set_shared_core_callbacks();
+func initSharedCore() {
+    set_shared_core_callbacks()
 }
