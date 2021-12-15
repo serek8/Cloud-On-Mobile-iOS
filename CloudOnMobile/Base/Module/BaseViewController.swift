7604860ae0eb3206e877051439ca95a8cd5d8269
Karol Piątek <karol.piatek@netguru.com>
2021-12-15 17:59:13 +0100

    Add base helpers

---
+++ CloudOnMobile/Base/Module/BaseViewController.swift
@@ -0,0 +1,98 @@
+//
+//  BaseViewController.swift
+//  PhoneDrive
+//
+//  Created by Karol P on 09/12/2021.
+//
+
+import UIKit
+
+class BaseViewController<Presenter: PresenterProtocol>: UIViewController, AutoKeyboard {
+    let containerView = UIView()
+
+    /// Instance of presenter.
+    let presenter: Presenter
+
+    // MARK: AutoKeyboard
+
+    var automaticallyAdjustKeyboardLayoutGuide = false {
+        willSet {
+            newValue ? startObservingKeyboardNotifications() : stopObservingKeyboardNotifications()
+        }
+    }
+
+    var onKeyboardWillAppear: ((Notification) -> Void)?
+
+    var onKeyboardWillDisappear: ((Notification) -> Void)?
+
+    var bottomConstraint: NSLayoutConstraint?
+
+    /// Initialize BaseViewController.
+    /// - Parameters:
+    ///   - presenter: Instance of presenter.
+    init(
+        presenter: Presenter,
+        backgroundColor: UIColor = .white
+    ) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+        view.backgroundColor = backgroundColor
+    }
+
+    @available(*, unavailable) required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        presenter.onViewDidLoad()
+        setupView()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        presenter.onViewWillAppear()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        presenter.onViewDidAppear()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        presenter.onViewDidDisappear()
+    }
+
+    // MARK: - Functions
+
+    /// Closes ViewController.
+    func closeView() {
+        if let navigationController = navigationController {
+            navigationController.popViewController(animated: true)
+        } else {
+            dismiss(animated: true)
+        }
+    }
+
+    /// - SeeAlso: Swift.deinit
+    deinit {
+        stopObservingKeyboardNotifications()
+    }
+}
+
+// MARK: - Private
+
+private extension BaseViewController {
+    func setupView() {
+        view.addSubview(containerView)
+        bottomConstraint = containerView.addConstraints { [
+            $0.equal(.top),
+            $0.equal(.leading),
+            $0.equal(.trailing),
+            $0.equal(.bottom)
+        ] }.last
+    }
+}
