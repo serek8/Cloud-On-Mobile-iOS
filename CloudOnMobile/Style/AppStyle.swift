93f186e66e253001c82b13e81dc30c89adda5fc2
Karol Piątek <karol.piatek@netguru.com>
2021-12-15 22:30:10 +0100

    Create AppStyle

--- CloudOnMobile/Style/AppStyle.swift
+++ CloudOnMobile/Style/AppStyle.swift
@@ -5,8 +5,20 @@
 //  Created by Karol P on 15/12/2021.
 //
 
-import Foundation
+import UIKit
 
-enum AppStyle {
-    case main
+final class AppStyle {
+    static var current = AppStyle()
+
+    let appFont: AppFont
+
+    let appColor: AppColor
+
+    init(
+        appColor: AppColor = DefaultAppColor(),
+        appFont: AppFont = DefaultAppFont()
+    ) {
+        self.appColor = appColor
+        self.appFont = appFont
+    }
 }
