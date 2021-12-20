3ecacac072e1e0e70c8a19a2e6c5b1a59bb552b2
Karol Piątek <karol.piatek@netguru.com>
2021-12-20 21:24:50 +0100

    Adjust background color

--- CloudOnMobile/Style/Color/AppColor.swift
+++ CloudOnMobile/Style/Color/AppColor.swift
@@ -20,7 +20,7 @@ struct DefaultAppColor: AppColor {
         case .white:
             return .white
         case .background:
-            return .black
+            return UIColor(red: 3 / 255, green: 14 / 255, blue: 29 / 255, alpha: 1)
         case .blue:
             return UIColor(red: 0.055, green: 0.439, blue: 0.945, alpha: 1)
         }
