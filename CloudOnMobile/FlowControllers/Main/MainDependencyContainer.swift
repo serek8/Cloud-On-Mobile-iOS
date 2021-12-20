7d35460f7a80b569ec0e52a318489295cdd85c11
Karol Piątek <karol.piatek@netguru.com>
2021-12-20 20:14:27 +0100

    Push MainViewController instead of storyboard

---
+++ CloudOnMobile/FlowControllers/Main/MainDependencyContainer.swift
@@ -0,0 +1,24 @@
+//
+//  MainDependencyContainer.swift
+//  CloudOnMobile
+//
+//  Created by Karol P on 20/12/2021.
+//
+
+import Foundation
+
+/// Dependency container for MainFlowController.
+protocol MainDependencyContainer {
+    /// Class responsible for connection with server.
+    var commandManager: CommandManager { get }
+}
+
+final class DefaultMainDependencyContainer: MainDependencyContainer {
+    lazy var commandManager: CommandManager = {
+        CommandManager()
+    }()
+
+    // MARK: - Private
+
+    private lazy var urlSession = URLSession(configuration: .ephemeral)
+}
---
+++ CloudOnMobile/FlowControllers/Main/MainFlowController.swift
@@ -0,0 +1,54 @@
+//
+//  MainFlowController.swift
+//  CloudOnMobile
+//
+//  Created by Karol P on 20/12/2021.
+//
+
+import UIKit
+
+final class MainFlowController: FlowController {
+    // MARK: - FlowController
+
+    var parent: FlowController?
+
+    var children: [FlowController] = []
+
+    let navigationController: UINavigationController
+
+    let presentation: FlowControllerPresentation
+
+    private let dependencyContainer: MainDependencyContainer
+
+    private let window: UIWindow?
+
+    /// Creates a new instance of a flow controller that is the root flow controller for the entire application.
+    ///
+    /// - Parameter window: key window of the application.
+    /// - Parameter dependencyContainer: container with app dependencies.
+    init(
+        window: UIWindow?,
+        dependencyContainer: MainDependencyContainer
+    ) {
+        self.dependencyContainer = dependencyContainer
+        navigationController = UINavigationController()
+        presentation = .root
+        self.window = window
+        presentMainScreen()
+    }
+
+    /// Presents the window on the screen.
+    func present() {
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
+
+private extension MainFlowController {
+    func presentMainScreen() {
+        let presenter = MainPresenter(commandManager: dependencyContainer.commandManager)
+        let viewController = MainViewController(presenter: presenter)
+        presenter.viewController = viewController
+        navigationController.setViewControllers([viewController], animated: false)
+    }
+}
