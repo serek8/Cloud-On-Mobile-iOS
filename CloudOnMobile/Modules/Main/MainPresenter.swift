a62329ff391249f6b24170f2143d1d6d60eeb1be
Karol Piątek <karol.piatek@netguru.com>
2021-12-20 19:01:09 +0100

    Add empty classes

---
+++ CloudOnMobile/Modules/Main/MainPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  MainPresenter.swift
+//  CloudOnMobile
+//
+//  Created by Karol P on 20/12/2021.
+//
+
+import Foundation
+
+final class MainPresenter: PresenterProtocol {
+    func refreshData() {}
+}
