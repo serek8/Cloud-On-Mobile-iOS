be8b01159254eec4c643e458af80d5d92730ff8d
Karol Piątek <karol.piatek@netguru.com>
2021-12-25 14:09:43 +0100

    Implement SheetPresentationController

---
+++ CloudOnMobile/Modules/Main/Files/FilesPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  FilesPresenter.swift
+//  CloudOnMobile
+//
+//  Created by Karol P on 22/12/2021.
+//
+
+import Foundation
+
+final class FilesPresenter: PresenterProtocol {
+    func refreshData() {}
+}
