2496a517d81906a77dd884a67013bcd5d7fa1e90
Karol Piątek <karol.piatek@netguru.com>
2021-12-26 11:23:24 +0100

    Add FileDownloader

---
+++ CloudOnMobile/Modules/Files/Models/File.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  CloudOnMobile
+//
+//  Created by Karol P on 26/12/2021.
+//
+
+import Foundation
+
+struct File {
+    /// Name of the file.
+    let name: String
+
+    /// Size of the file.
+    let size: Size
+
+    /// Type of the file.
+    let type: FileType
+}
