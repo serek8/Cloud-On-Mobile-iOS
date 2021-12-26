ccf6b63ecaac32b64d8b01c3113ec32840fc87a3
Karol Piątek <karol.piatek@netguru.com>
2021-12-26 10:11:21 +0100

    Add cells to files view

--- CloudOnMobile/Modules/Main/Files/FilesViewController.swift
+++ CloudOnMobile/Modules/Main/Files/FilesViewController.swift
@@ -8,6 +8,34 @@
 import UIKit
 
 final class FilesViewController: BaseViewController {
+    private var files: [IconTitleSubtitleView.ViewModel] = [
+        IconTitleSubtitleView.ViewModel(
+            icon: UIImage(),
+            title: "Title",
+            subtitle: "Subtitle"
+        ),
+        IconTitleSubtitleView.ViewModel(
+            icon: UIImage(),
+            title: "Title",
+            subtitle: "Subtitle"
+        ),
+        IconTitleSubtitleView.ViewModel(
+            icon: UIImage(),
+            title: "Title",
+            subtitle: "Subtitle"
+        ),
+        IconTitleSubtitleView.ViewModel(
+            icon: UIImage(),
+            title: "Title",
+            subtitle: "Subtitle"
+        ),
+        IconTitleSubtitleView.ViewModel(
+            icon: UIImage(),
+            title: "Title",
+            subtitle: "Subtitle"
+        )
+    ]
+
     private let tableView = with(UITableView()) {
         $0.rowHeight = UITableView.automaticDimension
         $0.separatorColor = .clear
@@ -29,11 +57,29 @@ final class FilesViewController: BaseViewController {
     }
 }
 
+// MARK: UITableViewDataSource
+
+extension FilesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        files.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = files[indexPath.row]
+        let cell = tableView.getReusableCellSafe(cellType: IconTitleSubtitleTableViewCell.self)
+        cell.fill(with: model)
+        return cell
+    }
+}
+
 // MARK: - Private
 
 private extension FilesViewController {
     func setupViews() {
         containerView.backgroundColor = AppStyle.current.color(for: .white)
         containerView.addSubview(tableView)
+
+        tableView.addConstraints { $0.equalEdges() }
+        tableView.dataSource = self
     }
 }
