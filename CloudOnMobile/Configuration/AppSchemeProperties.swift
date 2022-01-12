9a76d657e844f813ceb1e7f3c1dbe6198f749057
Jan Seredynski <janseredynski@gmail.com>
2022-01-12 10:23:47 +0100

    Fix merge conflicts

--- CloudOnMobile/Configuration/AppSchemeProperties.swift
+++ CloudOnMobile/Configuration/AppSchemeProperties.swift
@@ -10,6 +10,9 @@ import Foundation
 protocol AppSchemeProperties {
     /// Backend host for the app.
     var host: String { get }
+
+    /// Backend port for the app.
+    var port: Int { get }
 }
 
 /// Structure describes properties dependent on app scheme.
@@ -17,17 +20,21 @@ struct DefaultAppSchemeProperties: AppSchemeProperties {
     // MARK: - AppSchemeProperties
 
     let host: String
+    let port: Int
 
     // MARK: - Initialization
 
     /// Initialize app properties based on scheme.
     init() {
         #if DEVELOPMENT
-            host = "192.168.50.10"
+            host = "cloudon.cc"
+            port = 9283
         #elseif STAGING
-            host = "192.168.50.10"
+            host = "cloudon.cc"
+            port = 9283
         #elseif PRODUCTION
             host = "cloudon.cc"
+            port = 9293
         #endif
     }
 }
